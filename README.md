a63fca38972ae07509bab7aa2e5c63c5b2a525f3
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-07-30 22:42:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Check WebDriverManager
+
+Confirmation of [webdriver-manager](https://pypi.org/project/webdriver-manager/) operation.
+
+# Requirements
+
+- [Python >=3.7](https://www.python.org)
+
+# Setup
+
+```shell
+pip install -r requirements.txt
+```
+
+Start main.py.
+
+```shell
+python main.py
+```
+
+Automatically exits after a few seconds.
---
+++ main.py
@@ -0,0 +1,54 @@
+from time import sleep
+
+from rich.console import Console
+from rich.progress import track
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+console = Console()
+
+
+def main():
+    console.log("Launch Google Chrome.")
+
+    # Init
+    driver = init_webdriver()
+
+    # Access to Google.
+    driver.get("https://www.google.com")
+
+    # Wait
+    for _ in track(range(5), description="Waiting..."):
+        sleep(1)
+        pass
+
+    # Quit
+    driver.quit()
+
+    console.log("Quit Google Chrome.")
+
+
+def init_webdriver() -> webdriver.Chrome:
+    """Initialize ChromeWebDriver
+    Returns:
+        webdriver.Chrome: ChromeWebDriver
+    """
+    options = Options()
+    service = Service(ChromeDriverManager().install())
+
+    # Create
+    driver = webdriver.Chrome(options=options, service=service)
+
+    # Maximize
+    driver.maximize_window()
+
+    # Setting wait
+    driver.implicitly_wait(5)
+
+    return driver
+
+
+if __name__ == "__main__":
+    main()
